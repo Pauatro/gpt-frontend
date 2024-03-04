e3cdc24129e7f90295eab1008c8fb315bf302124
Jordi <jordijs6@gmail.com>
2024-03-04 19:32:20 +0100

    Edit App.tsx to include ConversationScreen using Router

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,20 @@ import { AppRoutes } from "./shared/router/routes";
 import { HomeScreen } from "./auth/ui/screens/HomeScreen";
 import { ConversationScreen } from "./auth/ui/screens/ConversationScreen";
 
-// function App() {
-//   return (
-//     <Routes>
-//       <Route element={<ProtectedRoute />}>
-//         <Route path={AppRoutes.Home} element={<HomeScreen />} />
-//       </Route>
-//       <Route element={<GuestRoute />}>
-//         <Route path={AppRoutes.Login} element={<LoginScreen />} />
-//       </Route>
-//     </Routes>
-//   );
-// }
 
 function App() {
   return (
-      <ConversationScreen />
+    <Routes>
+      <Route element={<ProtectedRoute />}>
+        <Route path={AppRoutes.Home} element={<HomeScreen />} />
+        <Route path={AppRoutes.Conversation} element={<ConversationScreen />} />
+      </Route>
+      <Route element={<GuestRoute />}>
+        <Route path={AppRoutes.Login} element={<LoginScreen />} />
+      </Route>
+    </Routes>
   );
 }
+
 
 export default App;
