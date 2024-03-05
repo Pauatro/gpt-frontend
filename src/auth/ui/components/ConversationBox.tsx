0139c011d3801a78f3b6c34b7a03a5be3d54616d
Jordi <jordijs6@gmail.com>
2024-03-05 12:52:24 +0100

    Refactor: remove ConversationButton component including it directly on ConversationBox

--- src/auth/ui/components/ConversationBox.tsx
+++ src/auth/ui/components/ConversationBox.tsx
@@ -1,14 +1,16 @@
-import { ConversationButton } from "./ConversationButton";
+import Icon from "./Icon.tsx";
 
 export const ConversationBox = () => {
   return (
     <div className="justify-self-center flex border rounded-2xl w-full md:max-w-3xl xl:max-w-5xl h-full px-4 pb-1">
-        <input
-          className="bg-transparent text-white placeholder:text-gray-500 w-full outline-none"
-          name="conversationBox"
-          placeholder="Type here to talk to the model..."
-        />
-        <ConversationButton />
+      <input
+        className="bg-transparent text-white placeholder:text-gray-500 w-full outline-none"
+        name="conversationBox"
+        placeholder="Type here to talk to the model..."
+      />
+      <button>
+        <Icon type="send" />
+      </button>
     </div>
   );
 };
--- src/auth/ui/components/ConversationButton.tsx
+++
@@ -1,9 +0,0 @@
-import Icon from "./Icon.tsx";
-
-export const ConversationButton = () => {
-  return (
-    <button>
-      <Icon type="send" /> 
-    </button>
-  );
-};
